ecd9a8b217ca31030ba35338282e0e9c8203bc88
lmuias <133999685+lmuias@users.noreply.github.com>
2023-12-12 22:49:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ const swiper = new Swiper('.slider-materials', {
     clickable: true
   },
 });
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+  const targetItem = e.target;
+
+  if (targetItem.closest('.icon-menu')) {
+    document.documentElement.classList.toggle('menu-open');
+  }
+}
