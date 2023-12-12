2a1c863b9bb30e68ead70362d6995f6b520b3434
lmuias <133999685+lmuias@users.noreply.github.com>
2023-12-12 20:45:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.slider-materials', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
